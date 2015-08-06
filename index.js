fedab8a948a862276f57dde1bfd1eea4a8250c6a
flames of love <mechanicofthesequence@gmail.com>
2015-08-06 03:18:50 +0200

    hack browser-pack

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 
 var path = require('path')
+var fs = require('fs')
 var moduleDeps = require('module-deps')
 var resolve = require('resolve')
-var Pack = require('nodepack')
 
 //hard coded modules that get replaced.
 //core modules or modules with c bits.
@@ -42,8 +42,30 @@ var deps = moduleDeps({
     }
   })
 
+// hacks to make browser-pack work with node
+var preludePath = path.join(__dirname, 'prelude.js')
+
+;(function () {
+  var bpackPath = path.join(__dirname, 'node_modules', 'browser-pack', 'index.js')
+  var src = fs.readFileSync(bpackPath, 'utf-8')
+  if (~src.indexOf('wrappedSource')) {
+    src = src.replace(/wrappedSource/g, 'warpedSource')
+    src = src.replace('function(require,module,exports){\\n', 'function(require,module,exports,__dirname,__filename){\\n')
+    src = src.replace(
+      "            ']'", [
+      "            ',\\n',",
+      "            JSON.stringify(row.id),",
+      "            ',\\n',",
+      "            JSON.stringify(path.dirname(row.id)),",
+      "            '\\n]'",
+      ].join('\n')
+    )
+    fs.writeFileSync(bpackPath, src)
+  }
+}())
+
 deps
-  .pipe(Pack({raw: true}))
+  .pipe(require('browser-pack')({raw: true, prelude: fs.readFileSync(preludePath, 'utf-8'), preludePath: preludePath}))
   .pipe(process.stdout)
 
 deps.end(process.argv[2] ? path.resolve(process.argv[2]) : __filename)
