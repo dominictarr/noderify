0fb5848b10af712ddaf9464da0b4d64a2986258f
Dominic Tarr <dominic.tarr@gmail.com>
2015-08-12 16:18:54 +0200

    fix: reimplement pack

--- pack.js
+++ pack.js
@@ -1,13 +1,26 @@
 
-
 function prelude (content, deps, entry) {
   var cache = {}
 
-  return (function newRequire (file) {
+  function load (file) {
     var d = deps[file]
-    var m = content[d[0]] //the actual module
+    if(cache[file]) return cache[d]
+    if(!d) return require(file)
+    var fn = content[d[0]] //the actual module
     var module = {exports: {}, parent: file !== entry}
-  })(entry)
+    return cache[file] = fn(
+      function (m) {
+        if(!deps[file][m]) return require(m)
+        else               return load (deps[file][m])
+      },
+      module,
+      module.exports,
+      file.substring(file.lastIndexOf('/')),
+      file
+    )
+  }
+
+  return load(entry)
 }
 
 module.exports = function (content, deps, entry) {
@@ -16,14 +29,15 @@ module.exports = function (content, deps, entry) {
     + (function () {
         var s = '{\n'
         for(var k in content) {
-          s = s
-          + JSON.stringify(k)+':\n'
-          + 'function (require, module, exports, __dirname, __filename) {\n'+
-          + content[k]
-          + '\n},\n'
+          s += [
+            JSON.stringify(k)+':\n', 
+            'function (require, module, exports, __dirname, __filename) {\n',
+            content[k],
+            '\n},\n'
+          ].join('')
         }
         return s+'\n}\n'
-      })
+      })()
     + ',\n'
     + JSON.stringify(deps, null, 2)
     + ',\n'
