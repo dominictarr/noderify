deafb35b3ae173edf29e7b6b17c49211ce0e3a12
Dominic Tarr <dominic.tarr@gmail.com>
2015-09-11 17:39:01 +1200

    output times taken to run tests

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@
 # version can still bundle noderify and get the exact same bundle.
 
 set -e # exit with an error if any command fails
-node index.js index.js > _b.js
-node _b.js index.js > _b2.js
+time node index.js index.js > _b.js
+time node _b.js index.js > _b2.js
 shasum _*.js
 
 diff _b.js _b2.js
